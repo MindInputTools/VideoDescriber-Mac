6e0f0455caf91ac3a63d6013461d81734daae44a
Mikael Holmgren Hallqvist <development@mindinputtools.com>
2026-03-10 23:03:47 +0100

    Initial Commit

---
+++ VideoDescriber/VideoDescriberApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoDescriberApp.swift
+//  VideoDescriber
+//
+//  Created by Mikael Holmgren Hallqvist on 2026-03-10.
+//
+
+import SwiftUI
+
+@main
+struct VideoDescriberApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
